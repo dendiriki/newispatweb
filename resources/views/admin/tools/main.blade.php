f557f72071b51734084bd3b0965f176d357c8ba2
dendiriki <dendirikirahmawan@gmail.com>
2023-07-14 10:51:49 +0700

    origin main

---
+++ resources/views/admin/tools/main.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Blog | Dashboard</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
+
+    <head>
+
+    </head>
+</head>
+
+<body>
+
+    @include('admin.tools.header')
+
+    <div class="container-fluid">
+        <div class="row">
+
+            @include('admin.tools.sidebar')
+
+
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                @yield('content')
+            </main>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
+
+    <script>
+        $(document).ready(function() {
+  $('#summernote').summernote();
+});
+    </script>
+
+</body>
+
+</html>
