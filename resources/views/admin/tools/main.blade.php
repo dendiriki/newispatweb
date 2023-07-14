46ea493d8eebced6c7f9a4505a9d7997a9c51401
dendiriki <dendirikirahmawan@gmail.com>
2023-07-14 10:52:58 +0700

    pindah ke main

--- resources/views/admin/tools/main.blade.php
+++ resources/views/admin/tools/main.blade.php
@@ -21,8 +21,6 @@
         <div class="row">
 
             @include('admin.tools.sidebar')
-
-
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                 @yield('content')
             </main>
