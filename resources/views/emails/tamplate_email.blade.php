32508121ff9566c710b3b5185b3842281b896c6a
dendiriki <dendirikirahmawan@gmail.com>
2023-10-16 22:56:53 +0700

    coba di rumah

--- resources/views/emails/tamplate_email.blade.php
+++ resources/views/emails/tamplate_email.blade.php
@@ -5,27 +5,93 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        th, td {
+            border: 1px solid black;
+            padding: 8px;
+            text-align: left;
+        }
+    </style>
 </head>
 <body>
     <h1>Ini Adalah Tes Email dari Program</h1>
     <p>saya mengirimakn email ini melalui program apakah bisa di</p>
     <p>jika email ini bisa di kirim ke pak hary dan mas vicky maka tahap berikutnya adalah mengirimkan data</p>
-    <p>Option       : {{ $customer->option }}</p>
-    <p>about       : {{ $customer->about }}</p>
-    <p>Name         : {{ $customer->name }}</p>
-    <p>Company      : {{ $customer->company }}</p>
-    <p>Email        : {{ $customer->email }}</p>
-    <p>Telphone     : {{ $customer->telphone }}</p>
-    <p>Massage      : {{ $customer->massage }}</p>
-    <p>Country      : {{ $customer->country }}</p>
-    <p>Location     : {{ $customer->location }}</p>
-    <p>Issue        : {{ $customer->issue }}</p>
-    <p>Grade 1      : {{ $customer->grade1 }}</p>
-    <p>Grade 2      : {{ $customer->grade2 }}</p>
-    <p>Grade 3      : {{ $customer->grade3 }}</p>
-    <p>Size         : {{ $customer->size }}</p>
-    <p>End App      : {{ $customer->end }}</p>
-    <p>Date         : {{ $customer->created_at }}</p>
+    <table>
+        <tr>
+            <th>Field</th>
+            <th>Value</th>
+        </tr>
+        <tr>
+            <td>Option</td>
+            <td>{{ $customer->option }}</td>
+        </tr>
+        <tr>
+            <td>About</td>
+            <td>{{ $customer->about }}</td>
+        </tr>
+        <tr>
+            <td>Name</td>
+            <td>{{ $customer->name }}</td>
+        </tr>
+        <tr>
+            <td>Company</td>
+            <td>{{ $customer->company }}</td>
+        </tr>
+        <tr>
+            <td>Email</td>
+            <td>{{ $customer->email }}</td>
+        </tr>
+        <tr>
+            <td>Telephone</td>
+            <td>{{ $customer->telphone }}</td>
+        </tr>
+        <tr>
+            <td>Message</td>
+            <td>{{ $customer->massage }}</td>
+        </tr>
+        <tr>
+            <td>Country</td>
+            <td>{{ $customer->country }}</td>
+        </tr>
+        <tr>
+            <td>Location</td>
+            <td>{{ $customer->location }}</td>
+        </tr>
+        <tr>
+            <td>Issue</td>
+            <td>{{ $customer->issue }}</td>
+        </tr>
+        <tr>
+            <td>Grade 1</td>
+            <td>{{ $customer->grade1 }}</td>
+        </tr>
+        <tr>
+            <td>Grade 2</td>
+            <td>{{ $customer->grade2 }}</td>
+        </tr>
+        <tr>
+            <td>Grade 3</td>
+            <td>{{ $customer->grade3 }}</td>
+        </tr>
+        <tr>
+            <td>Size</td>
+            <td>{{ $customer->size }}</td>
+        </tr>
+        <tr>
+            <td>End App</td>
+            <td>{{ $customer->end }}</td>
+        </tr>
+        <tr>
+            <td>Date</td>
+            <td>{{ $customer->created_at }}</td>
+        </tr>
+    </table>
 
 
 </body>
