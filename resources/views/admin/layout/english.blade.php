7ee0213ef87505aed84d11227bdde05eadf5eabf
dendiriki <dendirikirahmawan@gmail.com>
2023-10-21 15:32:19 +0700

    lanjut di komputer lagi males

--- resources/views/admin/layout/english.blade.php
+++ resources/views/admin/layout/english.blade.php
@@ -1,19 +1,18 @@
 @extends('admin.tools.main')
 
-
 @section('content')
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">{{ $title }}</h1>
     </div>
 
     @if (session()->has('success'))
-        <div class="alert alert-success " role="alert">
+        <div class="alert alert-success" role="alert">
             {{ session('success') }}
         </div>
     @endif
 
     <div class="table-responsive col-lg-12">
-        <a href="/admin/english/create" class="btn btn-primary mb-3"> Create New Post</a>
+        <a href="/admin/english/create" class="btn btn-primary mb-3">Create New Post</a>
         <table class="table table-striped table-sm">
             <thead>
                 <tr>
@@ -26,21 +25,17 @@
             <tbody>
                 @foreach ($posts as $post)
                     <tr>
-                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $loop->iteration + ($posts->currentPage() - 1) * $posts->perPage() }}</td>
                         <td>{{ $post->title }}</td>
                         <td>{{ $post->name }}</td>
                         <td>{{ $post->slug}}</td>
                         <td>
-                            <a href="/admin/english/{{ $post->slug }}" class="btn btn-info">view
-                                <span></span>
-                            </a>
-                            <a href="/admin/english/{{ $post->slug}}/edit" class="btn btn-warning">edit
-                                <span></span>
-                            </a>
+                            <a href="/admin/english/{{ $post->slug }}" class="btn btn-info">View</a>
+                            <a href="/admin/english/{{ $post->slug }}/edit" class="btn btn-warning">Edit</a>
                             <form action="/admin/english/{{ $post->slug}}" method="post" class="d-inline">
                                 @method('delete')
                                 @csrf
-                                <button class="border-0 btn btn-danger" onclick="return confirm('yakin mau hapus')">hapus</button>
+                                <button class="border-0 btn btn-danger" onclick="return confirm('Yakin mau hapus?">Hapus</button>
                             </form>
                         </td>
                     </tr>
@@ -49,5 +44,7 @@
             </tbody>
         </table>
     </div>
-    {{ $posts->links() }}
+    {{ $posts->appends(request()->input())->links() }}
+</div>
+
 @endsection
