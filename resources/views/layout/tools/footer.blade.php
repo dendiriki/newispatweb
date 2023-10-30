d3daf874982dd6c3cadd5cdb004d72f3e08a131f
dendiriki <dendirikirahmawan@gmail.com>
2023-10-30 11:09:19 +0700

    lanjut di kantro

--- resources/views/layout/tools/footer.blade.php
+++ resources/views/layout/tools/footer.blade.php
@@ -1,108 +1,101 @@
 <!-- info section -->
 <section class="info_section ">
     <div class="container">
-      <div class="row">
-        <div class="col-md-5">
-            <img src="{{ asset('assets/img/logo-ispat.png') }}" style="max-width: 3em; padding-bottom: 10px;">
-            <H4>PT. Ispat Indo</H4>
-            <p>
-                Ispat Indo, a steel industry pioneer, redefines excellence, driving innovation and setting global standards.
-            </p>
-        </div>
-        <div class="col-md-2 info_links">
-          <h5 style="font-weight: bold;
+        <div class="row">
+            <div class="col-md-5">
+                <img src="{{ asset('assets/img/logo-ispat.png') }}" style="max-width: 3em; padding-bottom: 10px;">
+                <H4>PT. Ispat Indo</H4>
+                <p>
+                    Ispat Indo, a steel industry pioneer, redefines excellence, driving innovation and setting global
+                    standards.
+                </p>
+            </div>
+            <div class="col-md-2 info_links">
+                <h5
+                    style="font-weight: bold;
           font-size: 18px;
           margin-bottom: 20px;
           color: #ccc;">
-            Mail To
-          </h5>
-          <ul>
-            <li style="margin-bottom: 5px;">
-              <a href="mailto:ispatindo@mittalsteel.com">
-                Ispat Indo
-              </a>
-            </li>
-            <li style="margin-bottom: 5px;">
-              <a href="mailto:marketing.indo@mittalsteel.com">
-                Sales & Marketing
-              </a>
-            </li>
-            <li style="margin-bottom: 5px;">
-              <a class="" href="mailto:custsupp.indo@mittalsteel.com">
-                CS Team
-              </a>
-            </li>
-          </ul>
-        </div>
+                    Mail To
+                </h5>
+                <ul>
+                    <li style="margin-bottom: 5px;">
+                        <a href="mailto:ispatindo@mittalsteel.com">
+                            Ispat Indo
+                        </a>
+                    </li>
+                    <li style="margin-bottom: 5px;">
+                        <a href="mailto:marketing.indo@mittalsteel.com">
+                            Sales & Marketing
+                        </a>
+                    </li>
+                    <li style="margin-bottom: 5px;">
+                        <a class="" href="mailto:custsupp.indo@mittalsteel.com">
+                            CS Team
+                        </a>
+                    </li>
+                </ul>
+            </div>
 
-        <div class="col-md-2 info_links">
-          <h5 style="color: transparent;">
-            .
-          </h5>
-          <ul>
+            <div class="col-md-2 info_links">
+                <h5 style="color: transparent;">
+                    .
+                </h5>
+                <ul>
 
-            <li style="margin-bottom: 5px;">
-              <a class="" href="mailto:hrd.indo@mittalsteel.com">
-                Careers
-              </a>
-            </li>
-            <li style="margin-bottom: 5px;">
-              <a class="" href="mailto:webmaster.indo@mittalsteel.com">
-                web master
-              </a>
-            </li>
-            <li style="margin-bottom: 5px;">
-              <a class="" href="https://web.whatsapp.com/send?phone=628155152222&text=Hi%2c%20Ispatindo.com">
-                Whatsapp
-              </a>
-            </li>
-          </ul>
-        </div>
+                    <li style="margin-bottom: 5px;">
+                        <a class="" href="mailto:hrd.indo@mittalsteel.com">
+                            Careers
+                        </a>
+                    </li>
+                    <li style="margin-bottom: 5px;">
+                        <a class="" href="mailto:webmaster.indo@mittalsteel.com">
+                            web master
+                        </a>
+                    </li>
+                    <li style="margin-bottom: 5px;">
+                        <a class=""
+                            href="https://web.whatsapp.com/send?phone=628155152222&text=Hi%2c%20Ispatindo.com">
+                            Whatsapp
+                        </a>
+                    </li>
+                </ul>
+            </div>
 
-        <div class="col-md-3">
-          <div class="info_social">
-            <h5 style="font-weight: bold;
+            <div class="col-md-3">
+                <div class="info_social">
+                    <h5
+                        style="font-weight: bold;
             font-size: 18px;
             margin-bottom: 20px;
             color: #ccc;">
-              Social Media
-            </h5>
-            <div class="social_container">
+                        Social Media
+                    </h5>
+                    <div class="social_container">
+                        <div>
+                            <a href="">
+                                <i class="fa-brands fa-twitter"></i>
+                            </a>
+                        </div>
+                        <div>
+                            <a href="https://www.linkedin.com/company/pt-ispat-indo">
+                                <i class="fa-brands fa-linkedin"></i>
+                            </a>
+                        </div>
 
-                <div>
-                  <a href="">
-                    <i class="fa-brands fa-facebook-f"></i>
-                  </a>
-                </div>
-                <div>
-                  <a href="">
-                    <i class="fa-brands fa-twitter"></i>
-                  </a>
-                </div>
-                <div>
-                  <a href="https://www.linkedin.com/company/pt-ispat-indo">
-                    <i class="fa-brands fa-linkedin"></i>
-                  </a>
-                </div>
-
-                <div>
-                  <a href="https://instagram.com/ispatindo_group/">
-                    <i class="fa-brands fa-instagram"></i>
-                  </a>
-                </div>
-                <div>
-                  <a href="">
-                    <i class="fa-brands fa-youtube"></i>
-                  </a>
-                </div>
-                <div>
-                  <a href="">
-                    <i class="fa-brands fa-tiktok"></i>
-                  </a>
-                </div>
-            </div>
-            <br>
-            {{-- <Div class="info_links">
+                        <div>
+                            <a href="https://instagram.com/ispatindo_group/">
+                                <i class="fa-brands fa-instagram"></i>
+                            </a>
+                        </div>
+                        <div>
+                            <a href="">
+                                <i class="fa-brands fa-youtube"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <br>
+                    {{-- <Div class="info_links">
 
               <div class="row">
               <div class="col">
@@ -121,14 +114,14 @@
             </div>
 
             </Div> --}}
-          </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </section>
-  <!-- end info_section -->
+</section>
+<!-- end info_section -->
 
-  <footer class="footer_section">
+<footer class="footer_section">
     <div class="container">
         <p>
             &copy; <span id="displayYear"></span> By
