0b5f44a6ba915c679051f9ae9cfdf2a63134c2af
daffa figiarsa <daffafigiarsa@gmail.com>
2023-12-27 09:13:40 +0700

    memperbarui card apply job (selesai)

--- resources/views/layout/karir/applyjob_indo.blade.php
+++ resources/views/layout/karir/applyjob_indo.blade.php
@@ -2,15 +2,24 @@
 
 @section('content')
     <section class="job_section layout_padding">
-        <div class="container">
-            <div class="heading_container col-md-12">
-                <div class="col row sub-heading">
-                    <h2 class="text-left">
-                        <!-- AVAILABLE POSITIONS -->
-                        careers
-                    </h2>
-                </div>
-            </div>
+    <div class="container">
+      <div class="heading_container col-md-12">
+        <div class="col row sub-heading">
+          <h2 class="text-left">
+            <!-- AVAILABLE POSITIONS -->
+            Career
+          </h2>
+        </div>
+        <p class="col-md-11"  style="opacity: 0.9;">
+        Di sini, di Ispat Indo Group, Anda akan bekerja dengan tim yang penuh dengan orang-orang berbakat yang saling mendukung dan menghargai kerja sama tim. Siap untuk mengambil langkah selanjutnya dalam karir Anda? Bergabunglah dengan kami sekarang!
+        </p>
+      </div>
+
+      <div class="col text-center" style="background-color: #3F58DD; padding: 15px 45px;">
+        <i class="fa-solid fa-triangle-exclamation" style="color: #fff; font-size: 4rem;"></i>
+        <h4 class="text-center" style="color: #fff; font-weight: 510;">Penipuan rekrutmen & peringatan penipuan</h4>
+        <p class="text-center" style="color: #fff; opacity: 0.8;">Kami tidak akan pernah meminta pertukaran uang atau detail kartu kredit dalam proses Rekrutmen. Harap waspadai aktivitas email yang mencurigakan dari orang yang berpura-pura sebagai perekrut atau individu senior di Ispat Indo Group. Jika ragu, harap abaikan pesan tersebut.</p>
+      </div>
 
             <div class="job_container">
                 <h4 class="job_heading">
@@ -21,60 +30,192 @@
 
 
                     @foreach ($jobs as $job)
-                        <div class="col-lg-6">
-                            <div class="box">
-                                <div class="job_content-box">
-                                    <div class="detail-box">
-                                        <h5>
-                                            {{ $job->name }}
-                                        </h5>
-                                        <div class="detail-info">
-                                            <div class="col">
-                                                <div class="row">
-                                                    <i class="fa-solid fa-dollar-sign"></i>
-                                                    <h6>
-                                                        <span>
-                                                            {{ $job->salary }}
-                                                        </span>
-                                                    </h6>
-                                                </div>
-                                                <div class="row">
-                                                    <i class="fa fa-solid fa-graduation-cap"></i>
-                                                    <h6>
-                                                        <span>
-                                                            {{ $job->tertiaryeducation }}
-                                                        </span>
-                                                    </h6>
-                                                </div>
-                                                <div class="row">
-                                                    <i class="fa fa-solid fa-briefcase"></i>
-                                                    <h6>
-                                                        <span>
-                                                            {{ $job->description }}
-                                                        </span>
-                                                    </h6>
-                                                </div>
-                                                <br>
-                                                <div class="row">
-                                                    <i class="fa fa-solid fa-calendar-days"></i>
-                                                    <h6 class="text-muted">
-                                                        <span>
-                                                            updated 5 days ago
-                                                        </span>
-                                                    </h6>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <br>
-                                <div class="option-box">
-                                    <a href="{{ $job->runninghour }}" class="apply-btn">
-                                        Apply Now
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
+
+
+        <div class="col-lg-6">
+            <div class="alert-availabe"></div> 
+            <div class="box">
+              <div class="job_content-box">
+                <div class="detail-box">
+                  <h5>
+                  {{ $job->name }}
+                  </h5>
+                  <div class="detail-info">
+                    <div class="col">
+                      <div class="row">
+                        <i class="fa-solid fa-dollar-sign"></i>
+                        <h6>
+                          <span>
+                          {{ $job->salary }}
+                          </span>
+                        </h6>
+                      </div>
+                      <div class="row">
+                        <i class="fa fa-solid fa-building"></i>
+                        <h6>
+                          <span>
+                            Financial & Accounting
+                          </span>
+                        </h6>
+                      </div>
+                      <div class="row">               
+                        <i class="fa fa-solid fa-hourglass-start"></i>
+                        <h6>
+                          <span>
+                            Full Time
+                          </span>
+                        </h6>
+                      </div>
+                      <div class="row">
+                        <i class="fa fa-solid fa-graduation-cap"></i>
+                        <h6>
+                          <span>
+                          {{ $job->tertiaryeducation }}
+                          </span>
+                        </h6>
+                      </div>
+                      <div class="row">
+                        <i class="fa fa-solid fa-briefcase"></i>
+                        <h6>
+                          <span>
+                          {{ $job->description }}
+                          </span>
+                        </h6>
+                      </div>
+                      <br>
+                      <div class="row">
+                        <i class="fa-regular fa-clock text-muted"></i>
+                        <h6 class="text-muted">
+                          <span>
+                            updated 5 days ago
+                          </span>
+                        </h6>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <br>
+              
+              <h6 style="padding-left: 12px; margin-bottom: 0px; color: #252525;">
+                <span>Status :</span>
+              </h6>
+
+              <div class="row">
+
+                <div class="col " style="margin-left: 25px;">
+                  <div class="row availabe-box">
+                    <i class="fa-solid fa-check"></i>
+                    <H6>Available</H6>
+                  </div>
+                </div>
+
+                <div class=" option-box">
+                  <a href="" class="apply-btn">
+                    Apply Now
+                  </a>
+                </div>
+                
+              </div>
+
+            </div>
+          </div>
+          
+
+
+
+          <!-- <div class="col-lg-6">
+            <div class="alert-unavailabe"></div>
+            <div class="box">
+              <div class="job_content-box">
+                <div class="detail-box">
+                  <h5>
+                  {{ $job->name }}
+                  </h5>
+                  <div class="detail-info">
+                    <div class="col">
+                      <div class="row">
+                        <i class="fa-solid fa-dollar-sign"></i>
+                        <h6>
+                          <span>
+                          {{ $job->salary }}
+                          </span>
+                        </h6>
+                      </div>
+                      <div class="row">
+                        <i class="fa fa-solid fa-building"></i>
+                        <h6>
+                          <span>
+                            Financial & Accounting
+                          </span>
+                        </h6>
+                      </div>
+                      <div class="row">               
+                        <i class="fa fa-solid fa-hourglass-start"></i>
+                        <h6>
+                          <span>
+                            Full Time
+                          </span>
+                        </h6>
+                      </div>
+                      <div class="row">
+                        <i class="fa fa-solid fa-graduation-cap"></i>
+                        <h6>
+                          <span>
+                          {{ $job->tertiaryeducation }}
+                          </span>
+                        </h6>
+                      </div>
+                      <div class="row">
+                        <i class="fa fa-solid fa-briefcase"></i>
+                        <h6>
+                          <span>
+                          {{ $job->description }}
+                          </span>
+                        </h6>
+                      </div>
+                      <br>
+                      <div class="row">
+                        <i class="fa-regular fa-clock text-muted"></i>
+                        <h6 class="text-muted">
+                          <span>
+                            updated 1 momth ago
+                          </span>
+                        </h6>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <br>
+              
+ 
+              <h6 style="padding-left: 12px; margin-bottom: 0px; color: #252525;">
+                <span>Status :</span>
+              </h6>
+
+              <div class="row">
+
+                <div class="col " style="margin-left: 25px;">
+                  <div class="row unavailabe-box">
+                    <i class="fa-solid fa-xmark"></i>
+                    <H6>Unavailable</H6>
+                  </div>
+                </div>
+
+                <div class=" option-box">
+                  <a href="" class="apply-btn">
+                    Apply Now
+                  </a>
+                </div>
+                
+              </div>
+
+
+            </div>
+          </div> -->
+
+
                     @endforeach
 
 
